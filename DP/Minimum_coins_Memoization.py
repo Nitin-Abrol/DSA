7d0d8a077f9190a02119fa2e74a18a7d9ec13050
Nitin Abrol <abroln39@gmail.com>
2022-04-05 12:55:45 +0530

    Update Minimum_coins_Memoization.py

--- DP/Minimum_coins_Memoization.py
+++ DP/Minimum_coins_Memoization.py
@@ -1,1 +1,23 @@
+def mcoins(i,target):
+    if i == 0:
+        if (target%coin[i] == 0):
+            return target/coin[i]
+        return 999999
+    if dp[i][target]!=-1:
+        return dp[i][target]
+    nottake = 0 + mcoins(i-1,target)
+    take = 999999
+    if coin[i] <= target:
+        take = 1 + mcoins(i,target-coin[i])
+        
+    dp[i][target] = int(min(take,nottake))
+    return dp[i][target]
+ 
+coin = [4,2,3,1]
+target = 91
+dp = [[-1 for i in range(target+1)]for j in range(len(coin))]
+print(mcoins(len(coin)-1,target))
 
+
+#OUTPUT
+#23
