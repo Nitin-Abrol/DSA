5d55bdf4bfb6b16c8114cc7566bcc7a9fb908b28
Nitin Abrol <abroln39@gmail.com>
2022-04-05 18:37:39 +0530

    Update Unbounded_KnapSack_Recursion.py

--- DP/Unbounded_KnapSack_Recursion.py
+++ DP/Unbounded_KnapSack_Recursion.py
@@ -1,1 +1,19 @@
+val = [5,11,13]
+wt = [2,4,6]
+cap = 10
 
+def unbounded(i,cap):
+    if i == 0:
+        return (cap//wt[0])*val[0]
+    
+    nottake = 0 + unbounded(i-1,cap)
+    take = -999999
+    if wt[i] <=  cap:
+        take = val[i] + unbounded(i,cap-wt[i])
+    return max(nottake,take)
+
+print(unbounded(2,cap))
+
+
+#OUTPUT
+#27
